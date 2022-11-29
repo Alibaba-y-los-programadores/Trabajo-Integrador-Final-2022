b1239a9ff1ae6c2e93a3b886d1467bbc3b9fcdb4
Gabriel <63930189+Gabibelt@users.noreply.github.com>
2022-11-28 21:48:44 -0300

    nuevos cambios probando

--- ProyectoTickets/src/proyectotickets/generadorCodigos.java
+++ ProyectoTickets/src/proyectotickets/generadorCodigos.java
@@ -1,22 +1,23 @@
-
 package proyectotickets;
+
 
 public class generadorCodigos {
     // Este subproceso genera las entradas totales con el cï¿½digo alfanumï¿½rico 
-	public static void entradastotales(String matriz[][]) {
-		String codigoletras;
-		String codigonums;
-		String codigotickets;
-		int dimletras;
+	public static void generadorCodigos(String matriz[][]) {
+            generadorCodigos generadorCodigos = new generadorCodigos();
+		private String codigoletras;
+		private String codigonums;
+		private String codigotickets;
+		private int dimletras;
 		int i;
 		int j;
 		int k;
-		int letraaleatoria;
-		String letras;
-		int numaleatorio;
-		String nums;
-		int totalentradas;
-		// creamos 2 arreglos que contienen los caracteres a generar
+		private int letraaleatoria;
+		private String letras;
+		private int numaleatorio;
+		private String nums;
+		private int totalentradas;
+		// creamos 2 array que contienen los caracteres a generar
 		letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		// Obtenemos el mero total de letras 
 		dimletras = letras.length();
@@ -30,10 +31,12 @@ public class generadorCodigos {
 				for (k=0;k<=2;k++) {
 					// Se Busca un nï¿½mero al azar entre el 1 y el nï¿½mero de letras 
                                         int numeroRandom = (int)(Math.random()*101);
-					letraaleatoria = Math.floor(Math.random()*dimletras);
+					letraaleatoria = (int)
+                                                Math.round(Math.random()*dimletras);
 					// Se concatenan 3 letras aleatorias
 					codigoletras = codigoletras.concat(letras.substring(letraaleatoria,letraaleatoria+1));
-					numaleatorio = Math.floor(Math.random()*9);
+					numaleatorio = (int)
+                                                Math.floor(Math.random()*9);
 					// Se concatenan 3 numeros aleatorios 
 					codigonums = codigonums.concat(nums.substring(numaleatorio,numaleatorio+1));
 				}
